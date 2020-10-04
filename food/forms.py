2cc1d03933be37d734c3bacc3b8d66d01edab47d
moinmir <moin@princeton.edu>
2020-10-04 20:19:26 +0530

    update account info

--- food/forms.py
+++ food/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
 from food.models import User
