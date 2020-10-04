9cc2a75cf79f939c0c3c93d1aa1a7ebdf19629b0
moinmir <moin@princeton.edu>
2020-10-04 19:29:57 +0530

    added logout, account page

--- food/__init__.py
+++ food/__init__.py
@@ -13,7 +13,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 # Initialize the extensions
 maps = GoogleMaps(app)
 db = SQLAlchemy(app)
+
 login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 bcrypt = Bcrypt(app)
 
